4a4b81894676053b3f22f66a8211fda2a3a5bc99
Maynor <dmaynor488@gmail.com>
2024-07-06 20:17:49 -0600

    starting the path of security

---
+++ SpringSecurityJWT/src/main/java/com/example/models/RoleEntity.java
@@ -0,0 +1,23 @@
+package com.example.models;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+@Entity
+@Table(name = "roles")
+public class RoleEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long Id;
+
+    @Enumerated(EnumType.STRING)
+    private ERole name;
+}
